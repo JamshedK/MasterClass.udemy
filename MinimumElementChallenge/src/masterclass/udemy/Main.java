b6a3579bbabf04da88a0c85e062e00e2e999c404
Jamshed <karimnazarovj@gmail.com>
2020-03-21 10:45:06 +0900

    minimumElementChallengeSolved

--- MinimumElementChallenge/src/masterclass/udemy/Main.java
+++ MinimumElementChallenge/src/masterclass/udemy/Main.java
@@ -1,8 +1,33 @@
 package masterclass.udemy;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Enter the size of your data");
+        int count = scanner.nextInt();
+        System.out.println(findMin(getIntegers(count)));
+    }
+
+    public static int [] getIntegers(int count){
+        int [] array = new int[count];
+        System.out.println("Input the numbers");
+        for(int i = 0; i < count; i++){
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
+
+    public static int findMin(int [] array){
+        int min = 0;
+        for(int i = 0; i < array.length; i++){
+            if(array[i] < array[min]){
+                min = i;
+            }
+        }
+        return array[min];
     }
 }
